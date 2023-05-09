e7f7608de44e9e24b5dc55e72d73dfb71487ace2
Tomek Leszczynski <hiszpania2018@gmail.com>
2023-05-09 22:26:03 +0200

    Set up patterns for inputs. Thanks to that the form is validated simultaneously. Pattern doesn't work properly with browser's autofilling option. Password validation yet to be done.

---
+++ script.js
@@ -0,0 +1,38 @@
+const submitBtn = document.querySelector('button')
+const inputs = document.querySelectorAll('input')
+
+// function validatePassword() {
+// 	const passInput = document.getElementById('password')
+// 	const passValidate = document.getElementById('password_validation')
+// 	const inputError = document.querySelector('.input-error')
+// 	if ((passInput.value === passValidate.value) && !passInput.value==='') {
+// 		console.log('ok')
+// 		passInput.value = ''
+// 		passValidate.value = ''
+// 	} else {
+// 		inputError.style.visibility = 'visible'
+// 	}
+// }
+
+// function handleInputs(element) {
+// 	element.setCustomValidity('')
+// 	if (element.validity.valid && element.value !== '') {
+// 		element.setCustomValidity('')
+// 	} else {
+// 		element.setCustomValidity('Improper Input Value.')
+// 		element.setAttribute('invalid', '')
+// 	}
+// }
+// function handleInputs(element) {
+// 	element.setCustomValidity('')
+// 	if (element.validity.valid && element.value !== '') {
+// 		console.log('ok')
+// 	} else {
+// 		element.setCustomValidity('Improper Input Value.')
+// 		element.setAttribute('invalid', '')
+// 	}
+// }
+
+// inputs.forEach(input => input.addEventListener('change', () => handleInputs(input)))
+
+// submitBtn.addEventListener('click', checkForm)
